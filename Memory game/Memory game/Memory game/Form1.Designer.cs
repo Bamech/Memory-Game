fe680d06450ae2c41669eaab3c814066608ffe45
JohnI <65335744+Bamech@users.noreply.github.com>
2021-04-12 21:37:56 +0200

    Add files via upload

---
+++ Memory game/Memory game/Memory game/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Memory_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p1a = new System.Windows.Forms.PictureBox();
+            this.p5a = new System.Windows.Forms.PictureBox();
+            this.p4a = new System.Windows.Forms.PictureBox();
+            this.p3a = new System.Windows.Forms.PictureBox();
+            this.p2a = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2a)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 447);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(734, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1.Enabled = false;
+            this.p1.Image = ((System.Drawing.Image)(resources.GetObject("p1.Image")));
+            this.p1.Location = new System.Drawing.Point(111, 161);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(81, 123);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p1.TabIndex = 2;
+            this.p1.TabStop = false;
+            this.p1.Click += new System.EventHandler(this.p1_Click);
+            this.p1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p1_MouseDown);
+            this.p1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p1_MouseUp);
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p2.Enabled = false;
+            this.p2.Image = ((System.Drawing.Image)(resources.GetObject("p2.Image")));
+            this.p2.Location = new System.Drawing.Point(236, 161);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(81, 123);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p2.TabIndex = 3;
+            this.p2.TabStop = false;
+            this.p2.Click += new System.EventHandler(this.p2_Click);
+            this.p2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p2_MouseDown);
+            this.p2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p2_MouseUp);
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p3.Enabled = false;
+            this.p3.Image = ((System.Drawing.Image)(resources.GetObject("p3.Image")));
+            this.p3.Location = new System.Drawing.Point(362, 161);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(81, 123);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p3.TabIndex = 4;
+            this.p3.TabStop = false;
+            this.p3.Click += new System.EventHandler(this.p3_Click);
+            this.p3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p3_MouseDown);
+            this.p3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p3_MouseUp);
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p4.Enabled = false;
+            this.p4.Image = ((System.Drawing.Image)(resources.GetObject("p4.Image")));
+            this.p4.Location = new System.Drawing.Point(476, 161);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(81, 123);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p4.TabIndex = 5;
+            this.p4.TabStop = false;
+            this.p4.Click += new System.EventHandler(this.p4_Click);
+            this.p4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p4_MouseDown);
+            this.p4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p4_MouseUp);
+            // 
+            // p5
+            // 
+            this.p5.BackColor = System.Drawing.Color.Transparent;
+            this.p5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p5.Enabled = false;
+            this.p5.Image = ((System.Drawing.Image)(resources.GetObject("p5.Image")));
+            this.p5.Location = new System.Drawing.Point(593, 161);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(81, 123);
+            this.p5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p5.TabIndex = 6;
+            this.p5.TabStop = false;
+            this.p5.Click += new System.EventHandler(this.p5_Click);
+            this.p5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p5_MouseDown);
+            this.p5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p5_MouseUp);
+            // 
+            // p1a
+            // 
+            this.p1a.BackColor = System.Drawing.Color.Transparent;
+            this.p1a.Cursor = System.Windows.Forms.Cursors.Default;
+            this.p1a.Image = ((System.Drawing.Image)(resources.GetObject("p1a.Image")));
+            this.p1a.Location = new System.Drawing.Point(111, 161);
+            this.p1a.Name = "p1a";
+            this.p1a.Size = new System.Drawing.Size(81, 123);
+            this.p1a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p1a.TabIndex = 7;
+            this.p1a.TabStop = false;
+            this.p1a.Visible = false;
+            // 
+            // p5a
+            // 
+            this.p5a.BackColor = System.Drawing.Color.Transparent;
+            this.p5a.Cursor = System.Windows.Forms.Cursors.Default;
+            this.p5a.Image = ((System.Drawing.Image)(resources.GetObject("p5a.Image")));
+            this.p5a.Location = new System.Drawing.Point(593, 161);
+            this.p5a.Name = "p5a";
+            this.p5a.Size = new System.Drawing.Size(81, 123);
+            this.p5a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p5a.TabIndex = 8;
+            this.p5a.TabStop = false;
+            this.p5a.Visible = false;
+            // 
+            // p4a
+            // 
+            this.p4a.BackColor = System.Drawing.Color.Transparent;
+            this.p4a.Cursor = System.Windows.Forms.Cursors.Default;
+            this.p4a.Image = ((System.Drawing.Image)(resources.GetObject("p4a.Image")));
+            this.p4a.Location = new System.Drawing.Point(476, 161);
+            this.p4a.Name = "p4a";
+            this.p4a.Size = new System.Drawing.Size(81, 123);
+            this.p4a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p4a.TabIndex = 9;
+            this.p4a.TabStop = false;
+            this.p4a.Visible = false;
+            // 
+            // p3a
+            // 
+            this.p3a.BackColor = System.Drawing.Color.Transparent;
+            this.p3a.Cursor = System.Windows.Forms.Cursors.Default;
+            this.p3a.Image = ((System.Drawing.Image)(resources.GetObject("p3a.Image")));
+            this.p3a.Location = new System.Drawing.Point(362, 161);
+            this.p3a.Name = "p3a";
+            this.p3a.Size = new System.Drawing.Size(81, 123);
+            this.p3a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p3a.TabIndex = 10;
+            this.p3a.TabStop = false;
+            this.p3a.Visible = false;
+            // 
+            // p2a
+            // 
+            this.p2a.BackColor = System.Drawing.Color.Transparent;
+            this.p2a.Cursor = System.Windows.Forms.Cursors.Default;
+            this.p2a.Image = ((System.Drawing.Image)(resources.GetObject("p2a.Image")));
+            this.p2a.Location = new System.Drawing.Point(236, 161);
+            this.p2a.Name = "p2a";
+            this.p2a.Size = new System.Drawing.Size(81, 123);
+            this.p2a.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.p2a.TabIndex = 11;
+            this.p2a.TabStop = false;
+            this.p2a.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(799, 532);
+            this.Controls.Add(this.p2a);
+            this.Controls.Add(this.p3a);
+            this.Controls.Add(this.p4a);
+            this.Controls.Add(this.p5a);
+            this.Controls.Add(this.p1a);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Memory Game";
+            this.Load += new System.EventHandler(this.memoryGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2a)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p1a;
+        private System.Windows.Forms.PictureBox p5a;
+        private System.Windows.Forms.PictureBox p4a;
+        private System.Windows.Forms.PictureBox p3a;
+        private System.Windows.Forms.PictureBox p2a;
+    }
+}
+
---
+++ Memory game/Memory game/Memory game/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_game
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer p1Sound = new SoundPlayer("snd/d1.wav");
+        SoundPlayer p2Sound = new SoundPlayer("snd/d2.wav");
+        SoundPlayer p3Sound = new SoundPlayer("snd/d3.wav");
+        SoundPlayer p4Sound = new SoundPlayer("snd/d4.wav");
+        SoundPlayer p5Sound = new SoundPlayer("snd/d5.wav");
+
+        int[] numbers = new int[1000];
+        bool gameInProgress = false;
+        int round = 0;
+        int counter = 0;  
+        
+        public void picturesOff ()
+        {
+            p1.Enabled = false;
+            p2.Enabled = false;
+            p3.Enabled = false;
+            p4.Enabled = false;
+            p5.Enabled = false;
+        } 
+
+        public void clickedButtom (int button)
+        {
+            if (gameInProgress)
+            {
+                counter++;
+                if (numbers[counter - 1] != button)
+                {
+                    picturesOff();
+                    round--;
+                    label1.Text = "End game. Your score: " + round.ToString();
+                    SoundPlayer endGame = new SoundPlayer("snd/koniec.wav");
+                    endGame.Play();
+                    gameInProgress = false;
+                }
+
+                if (counter == round)
+                {
+                    label1.Text = "Great!";
+                    sequence();
+                }
+            }
+        }
+
+        public void sequence ()
+        {
+            p1.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p1.bmp");
+            p2.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p2.bmp");
+            p3.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p3.bmp");
+            p4.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p4.bmp");
+            p5.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p5.bmp");
+
+            Application.DoEvents();
+            Thread.Sleep(1000);
+
+            int secondsToStarts = 1;
+            for(int i=secondsToStarts;i>0;i--)
+            {
+                label1.Text = "Game starts in " + i;
+                Application.DoEvents();
+                Thread.Sleep(1000);
+            }     
+
+            label1.Text = "Remember the sequences";
+            Application.DoEvents();
+            Thread.Sleep(1000);
+            
+            round++;
+            
+            for (int i=0; i<round; i++)
+            {
+                switch(numbers[i])
+                {
+                    case 1:
+                        {
+                            p1Sound.Play();
+                            p1a.Visible = true;
+                        } break;
+                    case 2:
+                        {
+                            p2Sound.Play();
+                            p2a.Visible = true;
+                        }
+                        break;
+                    case 3:
+                        {
+                            p3Sound.Play();
+                            p3a.Visible = true;
+                        }
+                        break;
+                    case 4:
+                        {
+                            p4Sound.Play();
+                            p4a.Visible = true;
+                        }
+                        break;
+                    case 5:
+                        {
+                            p5Sound.Play();
+                            p5a.Visible = true;
+                        }
+                        break;
+
+                }
+                Application.DoEvents();
+                Thread.Sleep(1200);
+                p1a.Visible = false;
+                p2a.Visible = false;
+                p3a.Visible = false;
+                p4a.Visible = false;
+                p5a.Visible = false;
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+            p1.Enabled = true;
+            p2.Enabled = true;
+            p3.Enabled = true;
+            p4.Enabled = true;
+            p5.Enabled = true;
+            label1.Text = "Repeat the sequences";
+            counter = 0;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void memoryGame_Load(object sender, EventArgs e)
+        {
+            Random randNum = new Random();
+            numbers = Enumerable
+                .Repeat(0, 1000)
+                .Select(i => randNum.Next(1, 5))
+                .ToArray();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {            
+            if(gameInProgress == false)
+            {
+                picturesOff();
+                gameInProgress = true;
+                SoundPlayer startSound = new SoundPlayer("snd/start.wav");
+                startSound.Play();                
+                sequence();                
+            }
+        }
+
+        private void p1_Click(object sender, EventArgs e)
+        {
+            clickedButtom(1);
+        }
+
+        private void p2_Click(object sender, EventArgs e)
+        {
+            clickedButtom(2);
+        }
+
+        private void p3_Click(object sender, EventArgs e)
+        {
+            clickedButtom(3);
+        }
+
+        private void p4_Click(object sender, EventArgs e)
+        {
+            clickedButtom(4);
+        }
+
+        private void p5_Click(object sender, EventArgs e)
+        {
+            clickedButtom(5);
+        }
+
+        private void p1_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p1a.bmp");
+            p1Sound.Play();         
+        }
+
+        private void p1_MouseUp(object sender, MouseEventArgs e)
+        {
+            p1.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p1.bmp");
+        }
+
+        private void p2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Image image2 = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p2a.bmp");
+            p2.Image = image2;
+            p2Sound.Play();
+        }
+
+        private void p2_MouseUp(object sender, MouseEventArgs e)
+        {
+            p2.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p2.bmp");
+        }
+
+        private void p3_MouseDown(object sender, MouseEventArgs e)
+        {
+            p3.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p3a.bmp");
+            p3Sound.Play();
+        }
+
+        private void p3_MouseUp(object sender, MouseEventArgs e)
+        {
+            p3.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p3.bmp");
+        }
+
+        private void p4_MouseDown(object sender, MouseEventArgs e)
+        {
+            p4.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p4a.bmp");
+            p4Sound.Play();
+        }
+
+        private void p4_MouseUp(object sender, MouseEventArgs e)
+        {
+            p4.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p4.bmp");
+        }
+
+        private void p5_MouseDown(object sender, MouseEventArgs e)
+        {
+            p5.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p5a.bmp");
+            p5Sound.Play();
+        }
+
+        private void p5_MouseUp(object sender, MouseEventArgs e)
+        {
+            p5.Image = Image.FromFile(@"C:\Users\Dom\Desktop\C# Projekty\Memory game\img\p5.bmp");
+        }
+    }
+}
